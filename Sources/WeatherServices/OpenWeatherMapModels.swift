c0c7c908b012961d150a256e76224e91f0c6cbbc
Norm Barnard <norm@normbarnard.com>
2020-02-08 18:13:51 -0500

    display types

--- Sources/WeatherServices/OpenWeatherMapModels.swift
+++ Sources/WeatherServices/OpenWeatherMapModels.swift
@@ -35,7 +35,6 @@ extension WeatherServices.OpenWeatherMap {
         }
     }
 
-
     struct Weather: Decodable {
         var id: Int
         var summary: String
@@ -51,11 +50,11 @@ extension WeatherServices.OpenWeatherMap {
     }
 
     struct Temperature: Decodable {
-        var temp: Double
-        var feelsLike: Double
-        var minTemp: Double
-        var maxTemp: Double
-        var pressure: Int
+        var temp: WeatherServices.Types.TemperatureValue
+        var feelsLike: WeatherServices.Types.TemperatureValue
+        var minTemp: WeatherServices.Types.TemperatureValue
+        var maxTemp: WeatherServices.Types.TemperatureValue
+        var pressure: WeatherServices.Types.BaroPressusre
         var humidity: Int
 
         private enum CodingKeys: String, CodingKey {
@@ -66,6 +65,22 @@ extension WeatherServices.OpenWeatherMap {
             case pressure
             case humidity
         }
+
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            let tempK = try container.decode(Double.self, forKey: .temp)
+            let feelsK = try container.decode(Double.self, forKey: .feelsLike)
+            let minK = try container.decode(Double.self, forKey: .minTemp)
+            let maxK = try container.decode(Double.self, forKey: .minTemp)
+            temp = .kelvin(tempK)
+            feelsLike = .kelvin(feelsK)
+            minTemp = .kelvin(minK)
+            maxTemp = .kelvin(maxK)
+            let baroInMetric = try container.decode(Int.self, forKey: .pressure)
+            pressure = .hpa(Double(baroInMetric))
+            humidity = try container.decode(Int.self, forKey: .humidity)
+        }
+
     }
 
     struct Coordinates: Decodable {
@@ -86,13 +101,21 @@ extension WeatherServices.OpenWeatherMap {
     }
 
     struct Wind: Decodable {
-        var speed: Double
+        var speed: WeatherServices.Types.Speed
         var direction: Double
 
         private enum CodingKeys: String, CodingKey {
             case speed
             case direction = "deg"
         }
+
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            let speedK = try container.decode(Double.self, forKey: .speed)
+            speed = .kph(speedK)
+            direction = try container.decode(Double.self, forKey: .direction)
+        }
+
     }
 
     struct Sys: Decodable {
@@ -110,9 +133,100 @@ extension WeatherServices.OpenWeatherMap {
             case oneHour = "1h"
             case threeHour = "3h"
         }
+        func imperial() -> VolumeOverTime {
+            let factor: Double = 0.0393701
+            return VolumeOverTime(
+                oneHour: oneHour * factor,
+                threeHour: threeHour * factor
+            )
+        }
+
     }
 
+}
+
+// MARK: CustomStringConvertible
+
+extension WeatherServices.OpenWeatherMap.CurrentConditions: CustomStringConvertible {
+    public var description: String {
+        let components = [
+            "\t" + coordinates.description,
+            "\t" + sysInfo.description,
+            weather.map { $0.description }.joined(separator: ","),
+            clouds?.description,
+            temperature.description,
+            wind?.description,
+            rain?.description,
+            snow?.description
+        ]
+        .compactMap { $0 }
+        .joined(separator: "\n")
+
+        return
+            """
+            Current Conditions for \(cityName), \(sysInfo.country)
+            \(components)
+            """
+    }
+}
+
+extension WeatherServices.OpenWeatherMap.Weather: CustomStringConvertible {
+    var description: String { "\(summary): \(details)" }
+}
 
 
+extension WeatherServices.OpenWeatherMap.Temperature: CustomStringConvertible {
+    var description: String {
+        return
+            """
+            Temperature:
+            \tCurrent: \(temp.asImperial)
+            \tFeels Like: \(feelsLike.asImperial)
+            \tMin: \(minTemp.asImperial) Max: \(maxTemp.asImperial)
+            \tHumidity: \(humidity)%
+            \tPressure: \(pressure.asImperial)
+            """
+    }
+}
 
+extension WeatherServices.OpenWeatherMap.Wind: CustomStringConvertible {
+    var description: String {
+        return
+            """
+            Wind:
+            \t\(speed.asImperial) at \(direction)°
+            """
+    }
+}
+
+extension WeatherServices.OpenWeatherMap.Coordinates: CustomStringConvertible {
+    var description: String {
+        return "(\(lat.formatted(to: 4))° Latitude, \(lon.formatted(to: 4))° Longitude)"
+    }
+}
+
+extension WeatherServices.OpenWeatherMap.Clouds: CustomStringConvertible {
+    var description: String { "Cloud cover: \(percent.formatted(to: 0))%" }
+}
+
+extension WeatherServices.OpenWeatherMap.VolumeOverTime: CustomStringConvertible {
+    var description: String {
+        let imperial = self.imperial()
+        return
+            """
+            1 Hour: \(imperial.oneHour) in/hour
+            3 Hour: \(imperial.threeHour) in/hour
+            """
+    }
+}
+
+extension WeatherServices.OpenWeatherMap.Sys: CustomStringConvertible {
+    var description: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeZone = TimeZone.current
+        dateFormatter.timeStyle = .medium
+        let fmtSunrise = dateFormatter.string(from: Date(timeIntervalSince1970: sunrise))
+        let fmtSunset = dateFormatter.string(from: Date(timeIntervalSince1970: sunset))
+        return "Sunrise: \(fmtSunrise) Sunset: \(fmtSunset)"
+    }
 }
--- Sources/WeatherServices/WeatherServices.swift
+++ Sources/WeatherServices/WeatherServices.swift
@@ -10,4 +10,113 @@ import Foundation
 public enum WeatherServices {
     public enum OpenWeatherMap { }
     public enum ServiceErrors { }
+    public enum Types { }
+}
+
+extension Double {
+    func formatted(to decimalPlaces: Int) -> String {
+        return String(format: "%.*f", decimalPlaces, self)
+    }
+}
+
+extension WeatherServices.Types {
+
+    enum TemperatureValue: CustomStringConvertible {
+        case kelvin(Double)
+        case celcius(Double)
+        case fahrenheit(Double)
+        var asImperial: TemperatureValue  {
+            switch self {
+            case .fahrenheit:
+                return self
+            case .celcius(let value):
+                return .fahrenheit(value * 9/5 + 32)
+            case .kelvin(let value):
+                return .fahrenheit((value - 273.15) * 9/5  + 32)
+            }
+        }
+        var asMetric: TemperatureValue {
+            switch self {
+            case .fahrenheit(let value):
+                return .celcius((value - 32) * 5 / 9)
+            case .celcius:
+                return self
+            case .kelvin(let value):
+                return .celcius(value - 273.15)
+            }
+        }
+
+        var description: String {
+            let unit: String
+            switch self {
+            case .kelvin(let value):
+                unit = "\(value.formatted(to: 1))°K"
+            case .celcius(let value):
+                unit = "\(value.formatted(to: 1))°C"
+            case .fahrenheit(let value):
+                unit = "\(value.formatted(to: 1))°F"
+            }
+            return unit
+        }
+    }
+
+    enum Speed: CustomStringConvertible {
+
+        static let conversionFactor: Double = 1.609
+
+        case kph(Double)
+        case mph(Double)
+
+        var description: String {
+            switch self {
+            case .mph(let value):
+                return "\(value.formatted(to: 1)) Mph"
+            case .kph(let value):
+                return "\(value.formatted(to: 1)) Kph"
+            }
+        }
+
+        var asImperial: Speed {
+            switch self {
+            case .kph(let value):
+                return .mph(value * 2.23694)
+            case .mph:
+                return self
+            }
+        }
+    }
+
+    enum BaroPressusre: CustomStringConvertible {
+        case hpa(Double)
+        case psi(Double)
+
+        var description: String {
+            switch self {
+            case .hpa(let value):
+                return "\(value.formatted(to: 1)) hPa"
+            case .psi(let value):
+                return "\(value.formatted(to: 1)) psi"
+            }
+        }
+
+        var asImperial: BaroPressusre {
+            switch self {
+            case .hpa(let value):
+                return .psi(value / 6895)
+            case .psi:
+                return self
+            }
+        }
+
+        var asMetric: BaroPressusre {
+            switch self {
+            case .hpa:
+                return self
+            case .psi(let value):
+                return .hpa(value * 6895)
+            }
+        }
+
+    }
+
 }
